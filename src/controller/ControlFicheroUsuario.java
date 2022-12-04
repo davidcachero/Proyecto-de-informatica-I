ebf9820da1c39841a3e7f2d3dfbdf4ff15cb043b
Aussar <Aussar@192.168.1.42>
2022-12-04 14:27:12 +0100

    Añadir clase Usuario y controladorFichero de usuario

---
+++ src/controller/ControlFicheroUsuario.java
@@ -0,0 +1,52 @@
+package controller;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.Vector;
+
+import models.Usuario;
+
+public class ControlFicheroUsuario {
+	File fichero;
+	Scanner scLector;
+	PrintWriter pwEscritor;
+	Usuario usuario;
+	
+	Vector<Usuario> listaUsuarios;
+	
+	public ControlFicheroUsuario (String rutaArchivo) { /// Leo el archivo de usuarios
+		fichero= new File(rutaArchivo);
+		
+		try {
+			scLector= new Scanner(new FileReader(fichero));
+			scLector.useDelimiter(";");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+public Vector<Usuario> leerUsuario(){
+		
+		listaUsuarios =new Vector<Usuario>();
+		
+		while(scLector.hasNextLine()) { ///Recojo los usuarios en un vector
+			
+
+			String id = scLector.next();
+			float saldo = Float.parseFloat(scLector.next());
+			usuario =new Usuario(id, saldo); ////Si esta definido?
+			listaUsuarios.add(usuario);
+			
+			scLector.nextLine();
+		}
+		return listaUsuarios;
+		}
+		
+}
+	
+	
+	
+
---
+++ src/models/Usuario.java
@@ -0,0 +1,40 @@
+package models;
+
+public class Usuario {
+	
+	private String id;
+	private float saldo;
+	
+	public Usuario(String id, float saldo) {
+		this.setId(id);
+		this.setSaldo(saldo);
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public float getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
+	
+	public boolean checkId (String id) {
+		return this.id == id;
+	}
+	
+	public void calcularSaldo (float saldo) {
+		if ((this.saldo - saldo)>0) 
+			this.saldo = this.saldo - saldo;
+		else
+			System.out.println("No hay suficiente saldo"); ///Cambiar esto??
+		
+	}
+	
+}
