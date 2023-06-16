ad5fb9c1f721a31c0ab00a13109d7891c77f2b1c
Victor <v.a.lantaron@gmail.com>
2023-06-16 16:28:48 +0200

    add intolerances class and file from other branch

---
+++ src/models/Intolerance.java
@@ -0,0 +1,40 @@
+package models;
+
+public class Intolerance {
+	
+	private int id;
+	private String nombre;
+	private String image;
+	
+	public Intolerance(int id, String nombre, String image) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.image = image;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}	
+
+}
